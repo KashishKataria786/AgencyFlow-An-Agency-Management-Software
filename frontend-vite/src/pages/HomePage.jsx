87860349a11f96d14f50f009a97686f15769963b
kashishkataria786 <offverse.business@gmail.com>
2025-12-26 18:43:02 +0530

    Basic Template with Auth controllers

---
+++ frontend-vite/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import LoadingSpinner from '../components/common/LoadingSpinner.jsx'
+import Layout from '../components/layout/Layout.jsx'
+
+const HomePage = () => {
+  return (
+    <Layout>
+      HomePage
+      <LoadingSpinner/>
+    </Layout>
+  )
+}
+
+export default HomePage
